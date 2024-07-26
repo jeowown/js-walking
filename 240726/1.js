9afe1b629b0e0c354ec6f316380f9e5a108fb05a
jeowown <happy160820@gmail.com>
2024-07-26 11:55:41 +0900

    2회차 JS 실습

---
+++ 240726/1.js
@@ -0,0 +1,8 @@
+function printFruits(a, b) {
+	return "제가 좋아하는 과일은 " + a + "," + b + "입니다.";
+}
+
+console.log(printFruits("사과", " 오렌지"));
+
+// printFruits 함수가 사과, 오렌지를 매개 변수로 받아
+// “제가 좋아하는 과일은 사과, 오렌지입니다.”의 문장을 콘솔에 출력
---
+++ 240726/3.js
@@ -0,0 +1,11 @@
+function repeatWord(str, n) {
+    for (let i = 0; i < n; i++) {
+        console.log(str);
+    }
+  }
+  
+  // 함수 실행
+(repeatWord("hello", 5));
+  
+// `repeatWord` 함수의 내용을 채워주세요.
+//  “hello”, 5를 넣어 `repeatWord`  함수를 실행시켜주세요.
